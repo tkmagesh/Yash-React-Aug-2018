1e261ce551b1371b1e334efefa82e04058317e23
Magesh <tkmagesh77@gmail.com>
2018-08-16 15:09:16 +0530

    calculator complete

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,16 +1,35 @@
 function add(){
 	function parseArg(n){
-		if (Array.isArray(n)){
-			var result = 0;
-			for(var index = 0, count = n.length; index < count; index++)
-				result += parseArg(n[index]);
-			return result;
-		}
+		if (Array.isArray(n)) return add.apply(undefined, n);
 		if (typeof n === 'function') return parseArg(n());
 		return isNaN(n) ? 0 : parseInt(n);
 	}
-	var result = 0;
-	for(var index = 0, count = arguments.length; index < count; index++)
-		result += parseArg(arguments[index]);
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+/*
+	As a method of an obj
+		this -> obj
+
+	As a function
+		this -> global (window)
+
+	//apply, bind & call
+
+	Using the call method of the function
+
+	Using the apply method of the function
+
+
+	sum(10,20,30,40)
+		10 + sum(20,30,40)
+				20 + sum(30,40)
+						30 + sum(40)
+								40
+
+*/
+
+
+
+
+
