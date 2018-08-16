3b1d36e72fd62d052b1b89323a38f8b900c061f9
Magesh <tkmagesh77@gmail.com>
2018-08-16 13:05:46 +0530

    calculator - solution - phase - 1

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,3 +1,16 @@
-function add(x,y){
-	return x + y;
+function add(){
+	function parseArg(n){
+		if (Array.isArray(n)){
+			var result = 0;
+			for(var index = 0, count = n.length; index < count; index++)
+				result += parseArg(n[index]);
+			return result;
+		}
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n);
+	}
+	var result = 0;
+	for(var index = 0, count = arguments.length; index < count; index++)
+		result += parseArg(arguments[index]);
+	return result;
 }
